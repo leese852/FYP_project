28b3e177e6458e620e3bb6fa2d16f6703284aa4d
leese852 <88673132+leese852@users.noreply.github.com>
2026-01-19 00:41:12 +0800

    update vue dish

--- Fronted/user-center-fronted/src/store/useLoginUserStore.ts
+++ Fronted/user-center-fronted/src/store/useLoginUserStore.ts
@@ -2,7 +2,7 @@ import { getCurrentUser, logout } from "@/api/user";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-//Pinia 是 Vue 的专属状态管理库，它允许你跨组件或页面共享状态。
+//Pinia 是 Vue 的专属状态管理库，它允许你跨组件或页面共享状态 類似java static。
 // 如果你熟悉组合式 API 的话，
 // 你可能会认为可以通过一行简单的 export const state = reactive({})
 // 来共享一个全局状态。
@@ -12,7 +12,7 @@ export const useLoginUserStore
     username: "未登录",
   });
 
-  //远程获取登录用户信息
+  //透過session远程获取登录用户信息
   async function fetchLoginUser() {
     try{
       const userData = await getCurrentUser();
@@ -24,13 +24,14 @@ export const useLoginUserStore
       console.log(error.message,"沒有保存用戶信息");
     }
   }
+
   //单独设置信息，并进行更新
   function setLoginUser(user: any) {
     loginUser.value = user;
   }
 
   async function logoutUser() {
-    const res = await logout();
+    await logout();
     setLoginUser({ username: "未登录" });
   }
 

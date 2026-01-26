887321d82a66f0bbe4beb6e7cb98ad377e0fb215
Fu Ka Wai <jackyfu1123@gmail.com>
2026-01-26 20:26:18 +0800

    Ka Wai

---
+++ Backend/user-center-backend/src/main/java/com/leese/usercenter/service/UserCartService .java
@@ -0,0 +1,15 @@
+package com.leese.usercenter.service;
+
+import com.leese.usercenter.model.dto.CartAddDTO;
+import com.leese.usercenter.model.dto.CartUpdateDTO;
+import com.leese.usercenter.model.vo.CartVO;
+
+import java.util.List;
+
+public interface UserCartService extends IService<Cart> {
+    void add(CartAddDTO dto);
+    List<CartVO> getCartList();
+    void updateQuantity(CartUpdateDTO dto);
+    void remove(Integer dishId, String dishFlavor);
+    void clear();
+}
